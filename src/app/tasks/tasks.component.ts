5b9bbfbf6728d21a4cf4d7d8829bf2f8c58cd12e
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2024-09-23 02:20:11 +0530

    Component Communication Deleting task using @output and emitting data

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -44,5 +44,9 @@ export class TasksComponent {
     
     return this.tasks.filter((task)=>task.userId===this.userId);
   }
+  onCompleteEvent(id:string){
+    console.log(id);
+    this.tasks=this.tasks.filter((task)=>task.id!==id);
+  }
 
 }
