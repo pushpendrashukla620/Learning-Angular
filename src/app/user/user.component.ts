237e97ac0af8774b6a330717ed1477d86f66659f
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2024-09-23 02:02:30 +0530

    Dynamic css styling wwith class binding( selected user button )

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,7 +15,7 @@ const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
 export class UserComponent {
 
   @Input({ required: true }) user!: User;
-
+  @Input({required:true}) selected!:boolean;
   @Output() select=new EventEmitter();
 
   get imagePath(){
